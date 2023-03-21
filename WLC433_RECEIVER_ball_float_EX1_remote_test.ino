83b30424ce51046d492d9a43d804fd8867538929
lakshyatech <60256230+lakshyatech@users.noreply.github.com>
2023-03-21 08:45:49 +0530

    Add files via upload

---
+++ WLC433_RECEIVER_ball_float_EX1_remote_test.ino
@@ -0,0 +1,68 @@
+// Water level control receiver software
+
+
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+int motorpin = 5;
+int motorstate = 0;
+int tankpin = 3;
+int tankstate = 1;
+int tankled = 4;
+unsigned long period = 8000000;
+unsigned long time_now = 0;
+unsigned long text = 0;
+unsigned long tanktimer = 0;
+unsigned long tankperiod = 30000;
+
+
+void setup() { 
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);
+  pinMode(motorpin, OUTPUT);
+  digitalWrite(motorpin, motorstate);
+  pinMode(tankpin, INPUT);
+  pinMode(tankled, OUTPUT);
+  digitalWrite(tankled, !tankstate); 
+
+  tanktimer = millis();
+  Serial.println("start ok");
+}
+  
+void loop(){ 
+  
+  if (mySwitch.available()) {
+      //Serial.println("got message");
+      text = mySwitch.getReceivedValue();
+      Serial.println(text);
+      mySwitch.resetAvailable();
+      
+   }
+  
+   if ((text == 1613224 && tankstate == 1) || text == 2016056){
+
+        digitalWrite(tankled, 1);
+        
+    if (motorstate == 0){
+        motorstate = 1;
+        digitalWrite(motorpin,motorstate);
+        }
+        //Serial.println("resetting timer");
+        time_now = millis();
+        text = 0;
+        digitalWrite(tankled, 0);
+      }   
+     
+      
+     if ( millis() >= tanktimer + tankperiod ) {
+        //Serial.println("checking tank water");
+        tankstate = digitalRead(tankpin);
+        digitalWrite(tankled, !tankstate);
+        tanktimer = millis();
+    }
+       if ((((millis() >= time_now + period) || (tankstate == 0)) && motorstate == 1) || text == 2016052 || text == 1613220) {
+          motorstate = 0;
+          digitalWrite(motorpin,motorstate);
+      }   
+   }
